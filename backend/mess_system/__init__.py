cc08106e91e1903b74d843fc8de9331c79f803c5
Anki <ankireddyvennapusa1@gmail.com>
2026-02-01 14:28:30 +0530

    fix

--- backend/mess_system/__init__.py
+++ backend/mess_system/__init__.py
@@ -1,12 +1,17 @@
-import pymysql
+import os
+from decouple import config
 
-pymysql.install_as_MySQLdb()
-
-# Monkey patch version to satisfy Django
-import MySQLdb
-if not hasattr(MySQLdb, 'version_info'):
-    MySQLdb.version_info = (1, 4, 3, "final", 0)
-# Depending on error, we might need a higher version. 
-# The error said "mysqlclient 2.2.1 or newer is required; you have 1.4.6".
-# So lets fake 2.2.1
-MySQLdb.version_info = (2, 2, 1, "final", 0)
+# Only use PyMySQL if explicitly configured or casually locally if available
+# But we don't want to crash if it's missing in production (Postgres)
+if config('USE_MYSQL', default=False, cast=bool):
+    try:
+        import pymysql
+        pymysql.install_as_MySQLdb()
+        
+        # Monkey patch version to satisfy Django
+        import MySQLdb
+        if not hasattr(MySQLdb, 'version_info'):
+            MySQLdb.version_info = (1, 4, 3, "final", 0)
+        MySQLdb.version_info = (2, 2, 1, "final", 0)
+    except ImportError:
+        pass
